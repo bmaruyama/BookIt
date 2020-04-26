a730d0a4f393f3a4f66b2316963b921d46701250
Brett Maruyama <bmaruyama@discretedata.ca>
2020-04-26 17:10:34 -0600

    Add options_menu resource with a SearchView accessible in the app bar. Add a search_title string to strings.xml. Add ic_search image asset with the standard search icon. Add onCreateOptionsMenu which associates the searchable config with the SearchView.

--- app/src/main/java/ca/discretedata/bookit/MainActivity.kt
+++ app/src/main/java/ca/discretedata/bookit/MainActivity.kt
@@ -5,9 +5,12 @@
 package ca.discretedata.bookit
 
 import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,5 +24,19 @@ class MainActivity : AppCompatActivity() {
                 // Perform search and store the data in the database.
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (menu != null) {
+            menuInflater.inflate(R.menu.options_menu, menu)
+
+            // Using the search manager, associate the searchable configuration with the menu's SearchView.
+            val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+            (menu.findItem(R.id.search).actionView as SearchView).apply {
+                setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            }
+        }
+
+        return true
     }
 }
