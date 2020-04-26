bbcd6159279b573a717f1f347a9f6aa83f678bcf
Brett Maruyama <bmaruyama@discretedata.ca>
2020-04-26 16:28:55 -0600

    Update the main activity to process a search intent. Add searchable xml configuration. Add search intent to the manifest file. Add search_hint to the strings resource file.

--- app/src/main/java/ca/discretedata/bookit/MainActivity.kt
+++ app/src/main/java/ca/discretedata/bookit/MainActivity.kt
@@ -4,6 +4,8 @@
 
 package ca.discretedata.bookit
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check for a search intent
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                // Perform search and store the data in the database.
+            }
+        }
     }
 }
