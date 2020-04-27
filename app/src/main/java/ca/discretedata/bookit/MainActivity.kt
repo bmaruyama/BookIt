48e3852b8ee01b6e28e6dcee81c775fd67e052e9
Brett Maruyama <bmaruyama@discretedata.ca>
2020-04-26 19:20:44 -0600

    Rename recyclerview to bookList for clarity of purpose in activity_main. In MainActivity, inflate a binding and use it set the RecyclerView adapter and layout manager as well as set the content root.

--- app/src/main/java/ca/discretedata/bookit/MainActivity.kt
+++ app/src/main/java/ca/discretedata/bookit/MainActivity.kt
@@ -9,14 +9,24 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.widget.SearchView
+import androidx.recyclerview.widget.GridLayoutManager
+import ca.discretedata.bookit.adapters.BookAdapter
+import ca.discretedata.bookit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val recyclerView = binding.bookList
+        recyclerView.adapter = BookAdapter()
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
 
         // Check for a search intent
         if (Intent.ACTION_SEARCH == intent.action) {
@@ -24,6 +34,8 @@ class MainActivity : AppCompatActivity() {
                 // Perform search and store the data in the database.
             }
         }
+
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
