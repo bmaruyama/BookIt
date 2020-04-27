f8fd27ed1ff9f840e2ca6ad364d8d1a89ae7fd1b
Brett Maruyama <bmaruyama@discretedata.ca>
2020-04-26 18:37:55 -0600

    Enable viewBinding on the app module. Add the BookAdapter class for the RecyclerView.

---
+++ app/src/main/java/ca/discretedata/bookit/adapters/BookAdapter.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020. Discrete Data Inc.
+ */
+
+package ca.discretedata.bookit.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
+import ca.discretedata.bookit.data.Book
+import ca.discretedata.bookit.databinding.BookListItemBinding
+import ca.discretedata.bookit.viewmodels.BookSearchViewModel
+
+class BookAdapter : ListAdapter<Book, BookAdapter.ViewHolder>(
+    BookDiffCallback()
+) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            BookListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: BookListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root)  {
+
+        private val viewModel: BookSearchViewModel? = null
+
+        fun bind(book: Book) {
+            with(binding) {
+                bookTitle.text = book.title
+                bookAuthors.text = book.authors
+                // TODO: leave jacket image display for later
+                if (book.jacketUrl.isNullOrEmpty()) {
+                    // An empty URL means only show title and authors
+                    bookJacket.visibility = View.GONE
+                    bookTitle.visibility = View.VISIBLE
+                    bookAuthors.visibility = View.VISIBLE
+                } else {
+                    // A valid URL means show the jacket only
+                    bookJacket.visibility = View.VISIBLE
+                    bookTitle.visibility = View.GONE
+                    bookAuthors.visibility = View.GONE
+                }
+            }
+        }
+    }
+}
+
+private class BookDiffCallback : DiffUtil.ItemCallback<Book>() {
+
+    override fun areItemsTheSame(
+        oldItem: Book,
+        newItem: Book
+    ): Boolean {
+        return oldItem.apiId == newItem.apiId
+    }
+
+    override fun areContentsTheSame(
+        oldItem: Book,
+        newItem: Book
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
